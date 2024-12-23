272e0026b2a8875649f9b2874b2cf041bc404877
prem-pk24 <premvp24@gmail.com>
2024-12-23 19:47:09 +0530

    all pages bugs completed

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,31 +1,36 @@
+// ===For Number Input ====
+const phoneInput = document.getElementById("phone-input");
+if (phoneInput) {
+  phoneInput.addEventListener("input", (event) => {
+    event.target.value = event.target.value.replace(/[^0-9]/g, "");
+  });
+}
 // ===Back to Top Button ===
 const backToTopButton = document.getElementById("back-to-top");
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 100) {
-    backToTopButton.style.display = "flex";
-  } else {
-    backToTopButton.style.display = "none";
-  }
-});
-backToTopButton.addEventListener("click", () => {
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth",
+if (backToTopButton) {
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 100) {
+      backToTopButton.style.display = "flex";
+    } else {
+      backToTopButton.style.display = "none";
+    }
   });
-});
+  backToTopButton.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  });
+}
 
 // ===For Date Input ===
 const dateInput = document.getElementById("date-input");
-dateInput.addEventListener("input", () => {
-  if (dateInput.value) {
-    dateInput.classList.add("has-value");
-  } else {
-    dateInput.classList.remove("has-value");
-  }
-});
-
-// ===For Number Input ====
-const phoneInput = document.getElementById("phone-input");
-phoneInput.addEventListener("input", (event) => {
-  event.target.value = event.target.value.replace(/[^0-9]/g, "");
-});
+if (dateInput) {
+  dateInput.addEventListener("input", () => {
+    if (dateInput.value) {
+      dateInput.classList.add("has-value");
+    } else {
+      dateInput.classList.remove("has-value");
+    }
+  });
+}
